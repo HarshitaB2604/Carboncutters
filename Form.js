ed4cb7a00d82653babe64c41f0850df97129965d
Harshita <57848574+HarshitaB2604@users.noreply.github.com>
2020-03-28 22:33:17 -0700

    Update Form.js

--- Form.js
+++ Form.js
@@ -3,13 +3,17 @@ import "./bootstrap/node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 export default class Form extends React.Component {
   state = {
+    phoneNumber: "",
     YearOfManufacture: "",
     manufacturer: "",
     model: "",
-    mpg: "",
-    password: ""
+    mpg: ""
   };
 
+  //object for storing all final info about the car
+  //let carData = {};
+
+  //
   change = e => {
     this.setState({
       [e.target.name]: e.target.value
@@ -24,45 +28,72 @@ export default class Form extends React.Component {
   render() {
     return (
       <form>
-        <input
-          name="YearOfManufacture"
-          placeHolder="Year of Manufacture"
-          value={this.state.YearOfManufacture}
-          onChange={e => this.change(e)}
-        />
+        <label>
+          Phone Number:
+          <br />
+          <input
+            name="YearOfManufacture"
+            placeHolder="XXXXXXXXXX"
+            value={this.state.phoneNumber}
+            onChange={e => this.change(e)}
+          />
+        </label>
 
         <br />
         <br />
-        <input
-          name="manufacturer"
-          placeHolder="Manufacturer"
-          value={this.state.manufacturer}
-          onChange={e => this.change(e)}
-        />
+        <label>
+          Year of Manufacture:
+          <br />
+          <input
+            name="YearOfManufacture"
+            placeHolder="Year of Manufacture"
+            value={this.state.YearOfManufacture}
+            onChange={e => this.change(e)}
+          />
+        </label>
 
         <br />
         <br />
-        <input
-          name="model"
-          placeHolder="Model"
-          value={this.state.model}
-          onChange={e => this.change(e)}
-        />
+        <label>
+          Manufacture:
+          <br />
+          <input
+            name="manufacturer"
+            placeHolder="Manufacturer"
+            value={this.state.manufacturer}
+            onChange={e => this.change(e)}
+          />
+        </label>
 
         <br />
         <br />
-        <input
-          name="mpg"
-          placeHolder="Miles Per Gallon"
-          value={this.state.mpg}
-          onChange={e => this.change(e)}
-        />
+        <label>
+          Model:
+          <br />
+          <input
+            name="model"
+            placeHolder="Model"
+            value={this.state.model}
+            onChange={e => this.change(e)}
+          />
+        </label>
 
         <br />
         <br />
-        <button onClick={e => this.onSubmit(e)} size="lg">
-          Submit
-        </button>
+        <label>
+          Miles Per Gallon:
+          <br />
+          <input
+            name="mpg"
+            placeHolder="Miles Per Gallon"
+            value={this.state.mpg}
+            onChange={e => this.change(e)}
+          />
+        </label>
+
+        <br />
+        <br />
+        <button onClick={e => this.onSubmit(e)}>Submit</button>
       </form>
     );
   }
